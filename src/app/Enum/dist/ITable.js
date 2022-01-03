d824cd800ba1126529821a3af1467376ca89a01d
adeosunsam <adeosunsamuel30@gmail.com>
2022-01-03 10:04:43 +0100

    refactor to Ngrx state management

--- src/app/Enum/dist/ITable.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ITable = void 0;
-var ITable;
-(function (ITable) {
-    ITable["vvip"] = "VVIP";
-    ITable["vip"] = "VIP";
-    ITable["regular"] = "REGULAR";
-})(ITable = exports.ITable || (exports.ITable = {}));
